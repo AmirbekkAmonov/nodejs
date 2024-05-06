e1df005cef7d88dbb489a43519a389eb9d89e150
amirbek <amonovamirbek1@gmail.com>
2024-05-06 21:09:35 +0500

    A

--- 9-dars/dars/index.js
+++ 9-dars/dars/index.js
@@ -1,2 +1,22 @@
-import expres from "express";
-import dotevn from "dotenv";
+
+import express from "express"
+
+import { router as registerRouter } from "./routes/users.route.js"
+import { router as blogRouter } from "./routes/blog.route.js"
+const app = express()
+
+app.use(express.json())
+
+
+app.use('/register', registerRouter)
+app.use('/blog', blogRouter)
+
+// app.post('/login ', (req, res) => {
+// })
+
+// app.use("/register", registerRoute)
+// app.use("/login", loginRoute)
+// app.use("/blog", loginRoute)
+
+
+app.listen(3000)
